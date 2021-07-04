6b04ecff2619caf8d721148d1e186da0201a05ba
User <User@DESKTOP-M0J2S6A>
2021-07-04 22:55:57 +0700

    test commit, jangan hiraukan

--- src/Main/TUBES_RumahSakit.java
+++ src/Main/TUBES_RumahSakit.java
@@ -17,6 +17,7 @@ public class TUBES_RumahSakit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("test ini");
     }
     
 }
